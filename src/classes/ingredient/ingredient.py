7fb4222df7cebdce4cc3d1b42d178931524c6c44
mthw07 <mbystritskiy@edu.hse.ru>
2026-06-03 11:10:21 +0000

    Ingredient class added

---
+++ src/classes/ingredient/ingredient.py
@@ -0,0 +1,27 @@
+class Ingredient:
+        def __init__(self, name: str, quantity: float, unit: str):
+            self.name = name
+            self._quantity = quantity
+            self.unit = unit
+    
+        @property
+        def quantity(self):
+            return self._quantity
+        
+        @quantity.setter
+        def quantity(self, value):
+            value = float(value)
+            if value <= 0:
+                raise ValueError("Количество должно быть положительным")
+            self._quantity = value
+    
+        def __str__(self):
+            return f"{self.name}: {self.quantity} {self.unit}"
+        
+        def __repr__(self):
+            return f"Ingredient('{self.name}', {self.quantity},'{self.unit}')"
+        
+        def __eq__(self, other: Ingredient):
+            if (other.name == self.name and other.unit == self.unit):
+                return True
+            return False
